f59ca9edafe005df04ab409c2c42fe7748c7d6da
Enara123 <silunienara@gmail.com>
2024-12-06 18:31:47 +0530

    Use process.env to hide db configs

--- auth-service/src/config/database.js
+++ auth-service/src/config/database.js
@@ -1,7 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("ntc_bus_booking", "root", "root", {
-  host: "127.0.0.1",
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: "mysql",
 });
 
