ea28df52a50a338d00d56f0de186222d9cadeecb
Enara123 <silunienara@gmail.com>
2024-12-26 12:46:37 +0530

    Add authMiddleware

--- schedule-management-service/src/index.js
+++ schedule-management-service/src/index.js
@@ -4,6 +4,7 @@ import cityRoutes from "./routes/cityRoutes.js";
 import routeRoutes from "./routes/routeRoutes.js";
 import scheduleTemplateRoutes from "./routes/scheduleTemplateRoutes.js";
 import scheduleRoutes from "./routes/scheduleRoutes.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -11,9 +12,12 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+
+app.use(authMiddleware);
+
 app.use("/bus", busRoutes);
 app.use("/cities", cityRoutes);
-app.use("/routes", routeRoutes);
+app.use("/route", routeRoutes);
 app.use("/schedule-template", scheduleTemplateRoutes);
 app.use("/schedule", scheduleRoutes);
 
