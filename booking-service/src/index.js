40a15aa4e877258fac3773f27d58dafee296df2c
Enara123 <silunienara@gmail.com>
2024-12-28 03:26:47 +0530

    Install cors

--- booking-service/src/index.js
+++ booking-service/src/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import bookingRoute from "./bookings/bookingRoutes.js";
 import authMiddleware from "../middleware/authMiddleware.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
