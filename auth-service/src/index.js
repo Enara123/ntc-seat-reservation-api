58673df7ce7704a5f16c985817e289d335994986
Enara123 <silunienara@gmail.com>
2024-12-30 19:24:47 +0530

    Update cors configs

--- auth-service/src/index.js
+++ auth-service/src/index.js
@@ -7,9 +7,9 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: 'https://silunienara.me',
-  methods: 'GET,POST,PUT,DELETE,OPTIONS',
-  allowedHeaders: 'Content-Type,Authorization',
+  origin: '*',
+  methods: '*',
+  allowedHeaders: '*',
   credentials: true,
 }));
 
